850de11dd784184f3ac9a6d58756ba79c994950c
Stefan Frijters <stefan.frijters@gmail.com>
2023-08-07 17:46:44 +0200

    Improve run detail visibility in light theme

--- SpeedrunTracker/Controls/RunPropertyControl.xaml.cs
+++ SpeedrunTracker/Controls/RunPropertyControl.xaml.cs
@@ -5,7 +5,6 @@ public partial class RunPropertyControl : ContentView
     public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(RunPropertyControl));
     public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value), typeof(string), typeof(RunPropertyControl));
     public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(nameof(ImageSource), typeof(string), typeof(RunPropertyControl));
-    public static readonly BindableProperty ValueTextColorProperty = BindableProperty.Create(nameof(ValueTextColor), typeof(Color), typeof(RunPropertyControl));
     public static readonly BindableProperty IsValueVisibleProperty = BindableProperty.Create(nameof(IsValueVisible), typeof(bool), typeof(RunPropertyControl));
 
     public string Title
@@ -24,12 +23,6 @@ public partial class RunPropertyControl : ContentView
     {
         get => (string)GetValue(ImageSourceProperty);
         set => SetValue(ImageSourceProperty, value);
-    }
-
-    public Color ValueTextColor
-    {
-        get => (Color)GetValue(ValueTextColorProperty);
-        set => SetValue(ValueTextColorProperty, value);
     }
 
     public bool IsValueVisible
