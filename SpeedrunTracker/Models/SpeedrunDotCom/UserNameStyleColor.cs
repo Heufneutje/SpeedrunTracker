fa35b3fabba261a28ca4cd8a9ddda1320e4ec730
Stefan Frijters <stefan.frijters@gmail.com>
2023-08-07 17:51:15 +0200

    Clarify why we're hardcoding light theme user name colors

--- SpeedrunTracker/Models/SpeedrunDotCom/UserNameStyleColor.cs
+++ SpeedrunTracker/Models/SpeedrunDotCom/UserNameStyleColor.cs
@@ -4,6 +4,7 @@ namespace SpeedrunTracker.Models.SpeedrunDotCom;
 
 public class UserNameStyleColor
 {
+    // We're ignoring the API value since speedrun.com doesn't allow you to set it (anymore) and a lot of colors that are returned are very hard to read when using the light theme.
     [JsonIgnore]
     public string Light { get; set; }
 
