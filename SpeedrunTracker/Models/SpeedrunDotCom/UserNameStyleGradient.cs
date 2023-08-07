58976a0bd6b9051c201b436b8527bdf918e0b3a4
Stefan Frijters <stefan.frijters@gmail.com>
2023-08-07 17:14:33 +0200

    Improve light theme

--- SpeedrunTracker/Models/SpeedrunDotCom/UserNameStyleGradient.cs
+++ SpeedrunTracker/Models/SpeedrunDotCom/UserNameStyleGradient.cs
@@ -4,7 +4,9 @@ namespace SpeedrunTracker.Models.SpeedrunDotCom;
 
 public class UserNameStyleGradient
 {
+    [JsonIgnore]
     public string Light { get; set; }
+
     public string Dark { get; set; }
 
     [JsonIgnore]
@@ -12,4 +14,12 @@ public class UserNameStyleGradient
 
     [JsonIgnore]
     public Color DarkColor => Color.FromArgb(Dark);
+
+    [JsonIgnore]
+    public Color ThemeColor => Application.Current.RequestedTheme == AppTheme.Dark ? DarkColor : LightColor;
+
+    public UserNameStyleGradient()
+    {
+        Light = "#000000";
+    }
 }
