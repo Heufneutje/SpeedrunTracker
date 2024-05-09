08d584158574dafb0cfb230a0e59b2b7cf0612b2
Stefan Frijters <stefan.frijters@gmail.com>
2024-05-09 17:57:20 +0200

    Work around speedrun.com's v1 API not reporting archived status

---
+++ SpeedrunTracker/Models/ExclusionCollection.cs
@@ -0,0 +1,23 @@
+﻿namespace SpeedrunTracker.Models;
+
+public class ExclusionCollection : Dictionary<string, List<string>>
+{
+    public static ExclusionCollection ParseExclusions(string configuredExclusions)
+    {
+        if (configuredExclusions == null)
+            return [];
+
+        return configuredExclusions
+            .Split(',', StringSplitOptions.RemoveEmptyEntries)
+            .Select(exclusion => exclusion.Split(':').Select(x => x.Trim()).ToArray())
+            .Aggregate(new ExclusionCollection(), (exclusions, parts) =>
+            {
+                if (!exclusions.ContainsKey(parts[0]))
+                    exclusions[parts[0]] = [];
+                exclusions[parts[0]].Add(parts[1]);
+                return exclusions;
+            });
+    }
+
+    public bool IsExcluded(string gameId, string itemId) => ContainsKey(gameId) && this[gameId].Contains(itemId);
+}
--- SpeedrunTracker/Services/SpeedrunData/GameService.cs
+++ SpeedrunTracker/Services/SpeedrunData/GameService.cs
@@ -1,14 +1,21 @@
-﻿using SpeedrunTracker.Services.LocalStorage;
+﻿using Microsoft.Extensions.Configuration;
+using SpeedrunTracker.Services.LocalStorage;
 
 namespace SpeedrunTracker.Services.SpeedrunData;
 
 public class GameService : BaseCachableService, IGameService
 {
     private readonly IGameRepository _gameRepository;
+    private readonly ExclusionCollection _archivedCategories;
+    private readonly ExclusionCollection _archivedVariables;
+    private readonly ExclusionCollection _archivedLevels;
 
-    public GameService(IGameRepository gameRepository, ICacheService cacheService) : base(cacheService)
+    public GameService(IGameRepository gameRepository, ICacheService cacheService, IConfiguration configuration) : base(cacheService)
     {
         _gameRepository = gameRepository;
+        _archivedCategories = ExclusionCollection.ParseExclusions(configuration["speedrun-dot-com:archived-categories"]);
+        _archivedVariables = ExclusionCollection.ParseExclusions(configuration["speedrun-dot-com:archived-variables"]);
+        _archivedLevels = ExclusionCollection.ParseExclusions(configuration["speedrun-dot-com:archived-levels"]);
     }
 
     public Task<Game> GetGameAsync(string gameId)
@@ -16,19 +23,19 @@ public class GameService : BaseCachableService, IGameService
         return GetCachedResourceAsync(gameId, CacheItemType.Games, _gameRepository.GetGameAsync(gameId));
     }
 
-    public Task<List<Category>> GetGameCategoriesAsync(string gameId)
+    public async Task<List<Category>> GetGameCategoriesAsync(string gameId)
     {
-        return GetCachedResourceAsync(gameId, CacheItemType.Categories, _gameRepository.GetGameCategoriesAsync(gameId));
+        return (await GetCachedResourceAsync(gameId, CacheItemType.Categories, _gameRepository.GetGameCategoriesAsync(gameId))).Where(x => !_archivedCategories.IsExcluded(gameId, x.Id)).ToList();
     }
 
-    public Task<List<Level>> GetGameLevelsAsync(string gameId)
+    public async Task<List<Level>> GetGameLevelsAsync(string gameId)
     {
-        return GetCachedResourceAsync(gameId, CacheItemType.Levels, _gameRepository.GetGameLevelsAsync(gameId));
+        return (await GetCachedResourceAsync(gameId, CacheItemType.Levels, _gameRepository.GetGameLevelsAsync(gameId))).Where(x => !_archivedLevels.IsExcluded(gameId, x.Id)).ToList();
     }
 
-    public Task<List<Variable>> GetGameVariablesAsync(string gameId)
+    public async Task<List<Variable>> GetGameVariablesAsync(string gameId)
     {
-        return GetCachedResourceAsync(gameId, CacheItemType.Variables, _gameRepository.GetGameVariablesAsync(gameId));
+        return (await GetCachedResourceAsync(gameId, CacheItemType.Variables, _gameRepository.GetGameVariablesAsync(gameId))).Where(x => !_archivedVariables.IsExcluded(gameId, x.Id)).ToList();
     }
 
     public Task<PagedData<List<Game>>> SearchGamesAsync(string name)
