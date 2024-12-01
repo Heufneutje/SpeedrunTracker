6fb854db1e3febcb70e9a20f20afe05fe9519071
Stefan Frijters <stefan.frijters@hotmail.com>
2024-12-01 23:39:30 +0100

    Fix user PB crash

--- SpeedrunTracker/Models/SpeedrunDotCom/GamePlatform.cs
+++ SpeedrunTracker/Models/SpeedrunDotCom/GamePlatform.cs
@@ -1,6 +1,9 @@
-﻿namespace SpeedrunTracker.Models.SpeedrunDotCom;
+﻿using System.Text.Json.Serialization;
+
+namespace SpeedrunTracker.Models.SpeedrunDotCom;
 
 public record GamePlatform : BaseSpeedrunModel, INamedSpeedrunModel
 {
+    [JsonPropertyName("name")]
     public required string Name { get; set; }
 }
