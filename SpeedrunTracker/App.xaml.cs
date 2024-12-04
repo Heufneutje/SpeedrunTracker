ce92e797e4a91a7fb8cfc83eb2c7619e4bf0c733
Stefan Frijters <stefan.frijters@hotmail.com>
2024-12-04 01:19:13 +0100

    Release 1.1.2

--- SpeedrunTracker/App.xaml.cs
+++ SpeedrunTracker/App.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class App : Application
 {
-    public const string Version = "1.1.1";
+    public const string Version = "1.1.2";
 
     public App(ILocalSettingsService settingsService)
     {
